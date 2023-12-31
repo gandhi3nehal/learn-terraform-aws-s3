baa816e4094dcf3ffa1d15b3da6de9d59d04ec7a
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-31 18:29:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0, < 5.17.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4"
-    }
-  }
-}
 
 provider "aws" {
   region = var.region
